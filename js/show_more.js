4b77b0ea76c23b43ce89c14c0e612c02dcb4e666
gtamoro <gtamoro@gmail.com>
2016-09-12 12:49:45 +1000

    Removed superfluous comments

--- js/show_more.js
+++ js/show_more.js
@@ -3,16 +3,13 @@
 $(document).ready(start);
 
 function start() {
-  // $(".readmore").click ({more_trigger: ".readmore", toggle_text: "#show-this-on-click", less_trigger: ".readless"}, show_more);
-  // $(".readless").click ({more_trigger: ".readmore", toggle_text: "#show-this-on-click", less_trigger: ".readless"}, show_less);
-  // $(".learnmore").click({more_trigger: ".learnmore", toggle_text: "#learnmoretext"}, show_more)
-
   $(".readmore").click (function() { show_more(event,".readmore", "#show-this-on-click", ".readless"); } );
   $(".readless").click (function() { show_less(event,".readmore", "#show-this-on-click", ".readless"); } );
   $(".learnmore").click(function() { show_more(event,".learnmore", "#learnmoretext"); } );
 }
 
-// this function requires three parameters:
+// these functions have the following parameters:
+//   event: to prevent the link behaving as it should, and load the link
 //   toggle_text: paragraph that will expand
 //   more_trigger: link that represents the "show more"
 //   less_trigger: link that represents the "show less"
@@ -22,11 +19,6 @@ function show_more(event,more_trigger,toggle_text,less_trigger) {
   $(toggle_text).slideDown();
   toggle_links(more_trigger,less_trigger);
 }
-
-// this function requires three parameters:
-//   toggle_text: paragraph that will collapse
-//   more_trigger: link that represents the "show more"
-//   less_trigger: link that represents the "show less"
 
 function show_less(event,more_trigger,toggle_text,less_trigger) {
   event.preventDefault();
