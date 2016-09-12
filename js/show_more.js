0fdd8e162b9da22e67ae4d72483d3dfa4fb44bf8
gtamoro <gtamoro@gmail.com>
2016-09-12 12:52:56 +1000

    More comments

--- js/show_more.js
+++ js/show_more.js
@@ -3,29 +3,29 @@
 $(document).ready(start);
 
 function start() {
-  $(".readmore").click (function() { show_more(event,".readmore", "#show-this-on-click", ".readless"); } );
-  $(".readless").click (function() { show_less(event,".readmore", "#show-this-on-click", ".readless"); } );
+  $(".readmore").click(function() { show_more(event,".readmore", "#show-this-on-click", ".readless"); } );
+  $(".readless").click(function() { show_less(event,".readmore", "#show-this-on-click", ".readless"); } );
   $(".learnmore").click(function() { show_more(event,".learnmore", "#learnmoretext"); } );
 }
 
 // these functions have the following parameters:
-//   event: to prevent the link behaving as it should, and load the link
+//   event: to prevent the link behaving as it should, and follow the URL
 //   toggle_text: paragraph that will expand
 //   more_trigger: link that represents the "show more"
 //   less_trigger: link that represents the "show less"
 
-function show_more(event,more_trigger,toggle_text,less_trigger) {
+function show_more(event, more_trigger, toggle_text, less_trigger) {
   event.preventDefault();
   $(toggle_text).slideDown();
   toggle_links(more_trigger,less_trigger);
 }
 
-function show_less(event,more_trigger,toggle_text,less_trigger) {
+function show_less(event, more_trigger, toggle_text, less_trigger) {
   event.preventDefault();
   $(toggle_text).slideUp("slow", function() { toggle_links(less_trigger,more_trigger);} );
 }
 
-function toggle_links(hide_trigger,show_trigger) {
+function toggle_links(hide_trigger, show_trigger) {
   $(hide_trigger).hide();
   $(show_trigger).show();
 }
